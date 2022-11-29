76835fc74278fd52c610c82b1987c36f7875587d
Kennedy Moranga <kennedy.moranga@student.moringaschool.com>
2022-11-30 01:19:57 +0300

    Finished Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let num1 = 31;
+let num2 =2;
+let multiply = num1 * num2;
+multiply;
+
+let random = Math.floor(Math.random() * 10)+1;
+random;
+
+let num3= 19;
+let num4=5;
+let mod =num3 % num4;
+mod;
+
+let max=Math.max(1, 7, 9, 11, 16,20);
+max;
